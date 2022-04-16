d0f6867ee2dc6a2f90dd131266eb0a741301c868
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-16 23:18:26 +0800

    feat(*): tidy up

--- DSTR-Assignment-Array/main.cpp
+++ DSTR-Assignment-Array/main.cpp
@@ -22,22 +22,6 @@ int main() {
     Tutor tutor = addingInterface(tutorV);
     addToBack(tutorV, tutor);
     DisplayTutor(tutorV);
-    /*std::vector<Tutor>::iterator it;
-    for (it = tutorV.begin(); it != tutorV.end(); it++) {
-        Tutor t = *it;
-        std::cout << t.ID << '\n';
-        std::cout << t.name << '\n';
-        std::cout << t.payRate << '\n';
-        std::cout << t.rating << '\n';
-        std::cout << t.phone << '\n';
-        std::cout << t.joinDate.ToString() << '\n';
-        std::cout << t.terminateDate.ToString() << '\n';
-        std::cout << t.center->ID << '\n';
-        std::cout << t.center->name << '\n';
-        std::cout << t.subject->ID << '\n';
-        std::cout << t.subject->name << '\n';
-    }
-    tutorToFile(tutorV, "test.txt");*/
-    
+
     return 0;
 }
