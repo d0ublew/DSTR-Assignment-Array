e62000d405bb16b67860ba7d95756a46b499b243
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-21 20:54:58 +0800

    feat(main): remove unnecessary header

--- DSTR-Assignment-Array/main.cpp
+++ DSTR-Assignment-Array/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <vector>
 
-#include "add.h"
 #include "admin_menu.h"
-#include "display.h"
 #include "file2struct.h"
-#include "sort.h"
 #include "tutor.h"
 
 const std::string TUTOR_FILE = "./data/tutor.txt";
@@ -17,13 +14,6 @@ std::vector<Center> _CENTER = fileToCenter(CENTER_FILE);
 int main() {
     std::vector<Tutor> tutorV = fileToTutor(TUTOR_FILE);
     startMenu(tutorV);
-
-    /* Tutor tutor = addingInterface(tutorV); */
-    /* addToBack(tutorV, tutor); */
-    /* std::cout << tutorV.size() << '\n'; */
-    /* std::cout << tutorV.back().ID << '\n'; */
-    /* tutorToFile(tutorV, TUTOR_FILE); */
-    /* DisplayTutor(tutorV); */
 
     return 0;
 }
