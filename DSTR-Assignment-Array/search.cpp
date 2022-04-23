438b02dfefac72be7b4bd548efe6d226e71cbe0f
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-23 15:09:54 +0800

    fix(search): unable to find the first element

--- DSTR-Assignment-Array/search.cpp
+++ DSTR-Assignment-Array/search.cpp
@@ -10,7 +10,7 @@ int binarySearch(std::vector<Tutor> &v, Tutor t,
                  int (*CompareFn)(Tutor &, Tutor &), int offset) {
     int start = 0;
     int end = (int)v.size() - 1;
-    while (start <= end && end > 0) {
+    while (start <= end && end >= 0) {
         int mid = (start + end) / 2;
         Tutor t2 = v.at(mid);
         int result = (*CompareFn)(t, t2);
