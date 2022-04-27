316452af9b740a86dc74834025789e74998099d0
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-04-27 16:57:50 +0800

    refactor(search)

--- DSTR-Assignment-Array/search.h
+++ DSTR-Assignment-Array/search.h
@@ -5,9 +5,35 @@
 
 #include "tutor.h"
 
-std::vector<Tutor *> searchByID(std::vector<Tutor *> &, std::string);
-std::vector<Tutor *> searchByRating(std::vector<Tutor *> &, float);
+/**
+ * @brief perform binary search on sorted array and returns index
+ *
+ * @param v specifies the sorted array to be searched
+ * @param x specifies the tutor to be matched
+ * @param CompareFn specifies the function to compare two tutors
+ *
+ * @return -1 if not found, otherwise returns the matching index
+ */
+int binarySearch(std::vector<Tutor *> &v, Tutor x, int (*CompareFn)(Tutor &, Tutor &));
 
-int binarySearch(std::vector<Tutor *> &v, Tutor x, int (*CompareFn)(Tutor &, Tutor &), int offset = 0);
+/**
+ * @brief search tutor
+ *
+ * @param v specifies the array to be searched
+ * @param t specifies the tutor to be matched
+ * @param CompareFn specifies the function to compare two tutors
+ *
+ * @return tutor array with matching results
+ */
 std::vector<Tutor *> searchTutor(std::vector<Tutor *> &v, Tutor t, int (*CompareFn)(Tutor &, Tutor &));
-std::vector<Tutor *> slice(std::vector<Tutor *> &, int, int);
+
+/**
+ * @brief slice vector given starting index and ending index [start, end]
+ *
+ * @param v specifies the vector be sliced
+ * @param start specifies the starting index
+ * @param end specifies the ending index
+ *
+ * @return sliced vector
+ */
+std::vector<Tutor *> slice(std::vector<Tutor *> &v, int start, int end);
