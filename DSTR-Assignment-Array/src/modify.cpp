e1a5159737b90faaf3482591afc76886361b3be0
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-05-18 21:54:45 +0800

    refactor

--- DSTR-Assignment-Array/src/modify.cpp
+++ DSTR-Assignment-Array/src/modify.cpp
@@ -32,113 +32,79 @@ void modifyTutor(std::vector<Tutor *> &tutorV, bool isAdmin)
             choice = getIntInput("Do you want to rate this tutor (1 = Yes / 0 = No): ");
         }
 
-        if (isChoiceInMenuRange(choice, 1))
-        {
-            if (!isAdmin)
-            {
-                if (choice == 1)
-                {
-                    while (true)
-                    {
-                        float rate;
-                        rate = getFloatInput("Please give your rate (1.0 ~ 5.0): ");
-                        if (!isRatingRateRangeValid(rate))
-                        {
-                            continue;
-                        }
+        if (!isChoiceInMenuRange(choice, 1)) continue;
 
-                        size_t count_rate = tutor->countRate;
-                        float tutor_rate = tutor->rating;
-                        tutor_rate *= count_rate;
-                        tutor_rate += rate;
-                        count_rate += 1;
-                        tutor_rate /= count_rate;
-                        tutor->rating = tutor_rate;
-                        tutor->countRate = count_rate;
-                        cout << "Thank you for your rating" << endl;
-                        Enter();
-                        return;
-                    }
-                }
-                else
-                {
-                    return;
-                }
-            }
-            if (choice == 1)
+        if (choice == 0) return;
+
+        if (!isAdmin && choice == 1)
+        {
+            while (true)
             {
-                std::cout << "Modify:\n";
-                std::cout << "1. Phone Number\n";
-                std::cout << "2. Address\n";
-                std::cout << "3. Termination Date\n";
-                std::cout << "0. Back\n";
-                bool valid = false;
-                while (!valid)
+                float rate;
+                rate = getFloatInput("Please give your rate (1.0 ~ 5.0): ");
+                if (!isRatingRateRangeValid(rate))
                 {
-                    int option = getIntInput("Choose what to be modified: ");
-                    if (isChoiceInMenuRange(option, 3))
-                    {
-                        if (option == 1)
-                        {
-                            string phone;
-                            while (true)
-                            {
-                                std::cout << "New Phone: ";
-                                std::getline(std::cin, phone);
-                                if (isPhoneFormatValid(phone))
-                                {
-                                    tutor->phone = phone;
-                                    valid = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    continue;
-                                }
-                            }
-                        }
-                        else if (option == 2)
-                        {
-                            std::cout << "New Address: ";
-                            std::getline(std::cin, tutor->address);
-                            valid = true;
-                        }
-                        else if (option == 3)
-                        {
-                            string date;
-                            while (true)
-                            {
-                                std::cout << "Terminate date: ";
-                                std::getline(std::cin, date);
-                                if (isDateValid(date))
-                                {
-                                    tutor->terminateDate = Date(date);
-                                    valid = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    continue;
-                                }
-                            }
-                        }
-                        else if (option == 0)
-                        {
-                            valid = true;
-                        }
-                    }
-                    else
-                    {
-                        continue;
-                    }
+                    continue;
                 }
-                std::cout << "Update successful\n";
+
+                size_t count_rate = tutor->countRate;
+                float tutor_rate = tutor->rating;
+                tutor_rate *= count_rate;
+                tutor_rate += rate;
+                count_rate += 1;
+                tutor_rate /= count_rate;
+                tutor->rating = tutor_rate;
+                tutor->countRate = count_rate;
+                cout << "Thank you for your rating" << endl;
                 Enter();
-            }
-            else if (choice == 0)
-            {
                 return;
             }
         }
+        std::cout << "Modify:\n";
+        std::cout << "1. Phone Number\n";
+        std::cout << "2. Address\n";
+        std::cout << "3. Termination Date\n";
+        std::cout << "0. Back\n";
+        while (true)
+        {
+            int option = getIntInput("Choose what to be modified: ");
+            if (!isChoiceInMenuRange(option, 3)) continue;
+            if (option == 1)
+            {
+                string phone;
+                while (true)
+                {
+                    std::cout << "New Phone: ";
+                    std::getline(std::cin, phone);
+                    if (isPhoneFormatValid(phone))
+                    {
+                        tutor->phone = phone;
+                        break;
+                    }
+                }
+            }
+            else if (option == 2)
+            {
+                std::cout << "New Address: ";
+                std::getline(std::cin, tutor->address);
+            }
+            else if (option == 3)
+            {
+                string date;
+                while (true)
+                {
+                    std::cout << "Terminate date: ";
+                    std::getline(std::cin, date);
+                    if (isDateValid(date))
+                    {
+                        tutor->terminateDate = Date(date);
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        std::cout << "Update successful\n";
+        Enter();
     }
 }
